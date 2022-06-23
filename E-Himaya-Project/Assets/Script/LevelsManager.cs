640b7b783bba85af4d40ad0472a54aeb5a1d8c3a
Osm01 <44633928+Osm01@users.noreply.github.com>
2022-06-23 12:48:50 +0100

    Add Script Lock Categorie

---
+++ E-Himaya-Project/Assets/Script/LevelsManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelsManager : MonoBehaviour
+{
+    [SerializeField] Button[] Levelbuttons;
+
+    private void Awake()
+    {
+        //
+        int Reachedlevel = PlayerPrefs.GetInt("Level",1);
+        Debug.Log(Reachedlevel);
+        for(int i=0;i<Levelbuttons.Length;i++)
+        {
+            if(i>Reachedlevel)
+            {
+                Levelbuttons[i].interactable = false;
+            }else
+            {
+                //count start from 0 so lock image is second child should be child 1
+                Levelbuttons[i].gameObject.transform.GetChild(1).GetComponent<Image>().gameObject.SetActive(false);
+            }
+        }
+    }
+    
+}
