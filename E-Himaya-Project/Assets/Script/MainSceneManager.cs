92fcf3e62aa2c5881e1ff981cf7d278b765b6df0
Osm01 <44633928+Osm01@users.noreply.github.com>
2022-06-28 14:40:21 +0100

    panel setting

--- E-Himaya-Project/Assets/Script/MainSceneManager.cs
+++ E-Himaya-Project/Assets/Script/MainSceneManager.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainSceneManager : MonoBehaviour
 {
+    [SerializeField] GameObject MainPanel;
+    [SerializeField] GameObject ButtonQuit;
+    [SerializeField] GameObject ButtonSetting;
+
     public void LoadSceneByIndex(int IndexScene)
     {
         SceneManager.LoadScene(IndexScene);
@@ -11,5 +16,14 @@ public class MainSceneManager : MonoBehaviour
     public void Loadpanel(GameObject L_Panel)
     {
         L_Panel.SetActive(true);
+    }
+    public void LoadpanelWithOpacity(GameObject L_Panel)
+    {
+        L_Panel.SetActive(true);
+        Color color = MainPanel.GetComponent<Image>().color;
+        color.a = 0.2f;
+        MainPanel.GetComponent<Image>().color = color;
+        ButtonQuit.SetActive(false);
+        ButtonSetting.SetActive(false);
     }
 }
